4e61df77451a2a008cfb1bc38d6b3e189f118320
thachpham <thachmpham@gmail.com>
2026-08-17 20:34:39 +0700

    update

--- tipc/usage/cluster4/entrypoint.sh
+++ tipc/usage/cluster4/entrypoint.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+# load tipc module
+if ! lsmod | grep -q tipc; then
+    modprobe tipc
+fi
+
+# setup kernel dynamic debug
+if ! mountpoint -q /sys/kernel/debug; then
+    mount -t debugfs none /sys/kernel/debug;
+fi
+
+# enable tipc debug log
+echo "module tipc +p" > /sys/kernel/debug/dynamic_debug/control
+
+# set tipc address
 tipc node set addr "${tipc_address}"
 tipc bearer enable media eth dev eth0
+
+# keep container running
 tail -f /dev/null
