91495aa67d108a30b4dadd3636541385bcc026fc
thachpham <thachmpham@gmail.com>
2026-09-14 08:28:53 +0700

    update

--- iproute2/2n1r/entrypoint_h1.sh
+++ iproute2/2n1r/entrypoint_h1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Add route: h1 -> rt -> h2
-ip route add 20.0.0.0/24 dev eth0 via 10.0.0.100
+ip route add 20.0.0.0/24 dev eth0 via 10.0.0.254
 
 # Start services
 /usr/sbin/rsyslogd
--- iproute2/2n1r/entrypoint_h2.sh
+++ iproute2/2n1r/entrypoint_h2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Add route: h2 -> rt -> h1
-ip route add 10.0.0.0/24 dev eth0 via 20.0.0.100
+ip route add 10.0.0.0/24 dev eth0 via 20.0.0.254
 
 # Start services
 /usr/sbin/rsyslogd
