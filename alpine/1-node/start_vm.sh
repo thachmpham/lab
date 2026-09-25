bcd78a9eb1a55c58bb55a34ae15053ea1cb1f607
thachpham <thachmpham@gmail.com>
2026-09-25 23:09:24 +0700

    update

--- alpine/1-node/start_vm.sh
+++ alpine/1-node/start_vm.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 
+disk_a="disk_a.qcow2"
+disk_b="disk_b.qcow2"
+
 qemu-system-x86_64 -name vm \
     -m 1024 \
     -smp cpus=2 \
-    -drive file=vm.qcow2 \
     -nographic \
+    -drive file=$disk_a \
+    -drive file=$disk_b \
     -netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
     -netdev bridge,br=br0,id=net1 -device virtio-net-pci,netdev=net1
