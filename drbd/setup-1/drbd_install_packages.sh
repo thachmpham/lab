81cef352826d3398b2082cce98b476c0c50bdb2a
thachpham <thachmpham@gmail.com>
2026-09-26 12:14:14 +0700

    update

--- drbd/setup-1/drbd_install_packages.sh
+++ drbd/setup-1/drbd_install_packages.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 # install packages
-ssh vm1 apk add drbd-utils lsblk parted
-ssh vm2 apk add drbd-utils lsblk parted
+ssh vm1 apk add drbd-utils lsblk parted e2fsprogs-extra
+ssh vm2 apk add drbd-utils lsblk parted e2fsprogs-extra
 
 echo 'export PATH=$PATH:/usr/lib/drbd' | ssh vm1 tee -a /root/.profile
 echo 'export PATH=$PATH:/usr/lib/drbd' | ssh vm2 tee -a /root/.profile
